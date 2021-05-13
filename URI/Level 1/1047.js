4a814bf6ff8b9bb4a2e113a049a2b9b9507807de
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-05-13 11:23:16 -0300

    URI - 1047

--- URI/Level 1/1047.js
+++ URI/Level 1/1047.js
@@ -1,55 +1,24 @@
 let input = require("fs").readFileSync("stdin", "utf-8");
 
-let [horaInicial, minInicial, horaFinal, minFinal] = input.split(' ')
+let [hourStart, minStart, hourEnd, minEnd] = input.split(' ')
     .map(value => parseInt(value));
 
-let hourStart = horaInicial + (minInicial * 60);
-let hourEnd = horaFinal + (minFinal / 60);
+if (!hourStart) hourStart = 0;
+if (!minStart) minStart = 0;
+if (!hourEnd) hourEnd = 0;
+if (!minEnd) minEnd = 0;
 
-console.log(hourStart / 60);
+let timeFinal = (hourEnd*60 + minEnd) - (hourStart*60 + minStart) ;
 
-let countHour = 0;
-let countMin = 0;
+if (timeFinal <= 0) 
+    timeFinal += 24*60;
 
-if (horaInicial && !minInicial && !horaFinal && !minFinal) {
-    minFinal = horaInicial;
-    horaInicial = 0;
-    horaFinal = 0;
-    minInicial = 0;
-}
+let hourFinal = parseInt(timeFinal / 60);
+let minFinal = timeFinal % 60;
 
 
-let counterHour = () => {
-    while (horaInicial !== horaFinal) {
-        horaInicial === 23 ? horaInicial = 0 : horaInicial++;
-        countHour++;
-    }
-}
-
-let counterMin = () => {
-    while (minInicial !== minFinal) {
-        if (minInicial === 59) {
-            minInicial = 0;
-            horaInicial++;
-        } else 
-            minInicial++;
-        countMin++;
-    }
-}
-
-if (horaInicial === horaFinal && minInicial === minFinal)
+if (hourStart === hourEnd && minStart === minEnd)
     console.log('O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)');
-else if (horaInicial === horaFinal && minInicial !== minFinal) {
-    counterMin();
-    console.log('O JOGO DUROU 24 HORA(S) E ' + countMin + ' MINUTO(S)');
-} else if (horaInicial !== horaFinal && minInicial === minFinal) {
-    counterHour();
-    console.log('O JOGO DUROU ' + countHour  + ' HORA(S) E 0 MINUTO(S)');
-} else {
-    counterMin();
-    counterHour();
-    console.log(
-        'O JOGO DUROU '+ countHour + 
-        ' HORA(S) E ' + countMin + ' MINUTO(S)'
-    );
-}
+else
+    console.log('O JOGO DUROU ' + hourFinal + 
+        ' HORA(S) E ' + minFinal + ' MINUTO(S)');
