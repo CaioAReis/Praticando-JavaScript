5d2b665ccc8a32540a2b031c38cea1c0c2460514
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-05-20 11:43:22 -0300

    URI - segunda tentativa 1061

--- URI/Level 1/1061.js
+++ URI/Level 1/1061.js
@@ -1,30 +1,30 @@
 let input = require("fs").readFileSync("stdin", "utf-8");
 
-let [dayStart, hourStart, dayEnd, hourEnd] = input.split('\n');
+let [dayStart = 0, hourStart = 0, dayEnd = 0, hourEnd = 0] = input.split('\n');
 
-dayStart = parseInt(dayStart.replace('Dia ', '').replace(',','.'));
-[hourStart, minStart, secStart] = hourStart.split(' : ')
+dayStart = parseInt(dayStart.replace('Dia ', '').replace(',',''));
+[hourStart = 0, minStart = 0, secStart = 0] = hourStart.split(' : ')
     .map(value => parseInt(value));
 
-dayEnd = parseInt(dayEnd.replace('Dia ', '').replace(',','.'));
-[hourEnd, minEnd, secEnd] = hourEnd.split(' : ')
+dayEnd = parseInt(dayEnd.replace('Dia ', '').replace(',',''));
+[hourEnd = 0, minEnd = 0, secEnd = 0] = hourEnd.split(' : ')
     .map(value => parseInt(value));
 
-let timeFinal = (hourEnd*60 + minEnd + secEnd/60) - (hourStart*60 + minStart + secStart/60);
+let timeFinal = 
+(((dayEnd*24)*60) + hourEnd*60 + minEnd + secEnd/60) - (((dayStart*24)*60) + hourStart*60 + minStart + secStart/60);
 
 if (timeFinal <= 0)
     timeFinal += 24*60;
 
-let hourEvent = parseInt(timeFinal / 60);
-let minEvent = parseInt(timeFinal % 60);
+console.log((timeFinal/60)/24);
+console.log((timeFinal/60)%24);
+let dayEvent = parseInt((timeFinal/60)/24);
+let hourEvent = parseInt(((timeFinal/60)%24));
+let minEvent = parseInt(((timeFinal/60)%24) * 60);
 let secEvent = parseInt(minEvent / 60);
-let dayEvent = dayEnd - dayStart;
 
-if (hourEvent >= 24) {
-    dayEvent++;
-    hourEvent = 0;
-} else 
-    dayEvent--;
+// if (hourEvent >= 24) hourEvent = 0;
+// else dayEvent--;
 
 console.log(dayEvent + ' dia(s)');
 console.log(hourEvent + ' hora(s)');
