d52c1bfc1163628aecbe88666cce3d017a4f5953
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-06-04 13:46:40 -0300

    Refatorando - URI - 1071

--- URI/Level 1/1071.js
+++ URI/Level 1/1071.js
@@ -2,10 +2,15 @@ let input = require("fs").readFileSync("stdin", "utf-8");
 
 let [ X, Y ] = input.split('\n').map(value => parseInt(value));
 let sum = 0;
+let xNegative = false, yNegative = false;
 
-
+if (X < 0) xNegative = true;
+if (Y < 0) yNegative = true;
 
 if (X !== Y) {
+    X = Math.abs(X);
+    Y = Math.abs(Y);
+
     if (Y < X) {
         do {
             if (Math.abs(Y % 2) !== 0)
@@ -13,6 +18,9 @@ if (X !== Y) {
            
             Y++;
         } while (Y < X);
+
+        if (xNegative) console.log(-sum);
+        else console.log(sum);
     } else {
         do {
             if (Math.abs(X % 2) !== 0)
@@ -20,7 +28,9 @@ if (X !== Y) {
            
             X++;
         } while (X < Y);
+
+        if (yNegative) console.log(-sum);
+        else console.log(sum);
     }
-}
- 
-console.log(sum);
+} else 
+    console.log(sum);
