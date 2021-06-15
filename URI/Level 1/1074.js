56cb02223f694b44985f98b0ddb0d6d9c0755224
Caio Almeida <50685017+CaioAReis@users.noreply.github.com>
2021-06-15 20:45:15 -0300

    Update 1074.js

--- URI/Level 1/1074.js
+++ URI/Level 1/1074.js
@@ -1,7 +1,7 @@
 let input = require("fs").readFileSync("stdin", "utf-8");
 
 const values = input.split('\n').map(value => 
-    parseInt(value.replace(',', '.')));
+    parseInt(value.replace(',', '\n')));
 
 values.shift();
 
@@ -10,7 +10,7 @@ values.forEach(value => {
         if (value > 0) {
             if (value % 2 === 0) console.log("EVEN POSITIVE");
             else console.log("ODD POSITIVE");
-        } else {
+        } else if (value < 0) {
             if (value % 2 === 0) console.log("EVEN NEGATIVE");
             else console.log("ODD NEGATIVE");
         }
